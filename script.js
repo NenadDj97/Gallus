2cb87613bf1d41f58624f6228c931cc654beea3c
Nenad Đorđević <93268425+NenadDj97@users.noreply.github.com>
2022-05-19 12:38:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var within_first_modal = false;
+$('.btn-second-modal').on('click', function() {
+  if ($(this).hasClass('within-first-modal')) {
+    within_first_modal = true;
+    $('#first-modal').modal('hide');
+  }
+  $('#second-modal').modal('show');
+});
+
+$('.btn-second-modal-close').on('click', function() {
+  $('#second-modal').modal('hide');
+  if (within_first_modal) {
+    $('#first-modal').modal('show');
+    within_first_modal = false;
+  }
+});
+
+$('.btn-toggle-fade').on('click', function() {
+  if ($('.modal').hasClass('fade')) {
+    $('.modal').removeClass('fade');
+    $(this).removeClass('btn-success');
+  } else {
+    $('.modal').addClass('fade');
+    $(this).addClass('btn-success');
+  }
+});
